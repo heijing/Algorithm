3e3a009881b092accec7f10404597d7380b319a2
heijing <vickyleewhy@hotmail.com>
2015-05-28 21:00:01 -0400

    first commint

--- src/Decode.java
+++ src/Decode.java
@@ -1,24 +1,38 @@
-public class countAndSay{
-	public String countAndSay(int n) {
-        if(n == 0)
-        return "";
-        String res = "1";
-        int count = 1;
-        for(int i=1;i<n;i++){
-            StringBuilder item = new StringBuilder();
-            for(int j=0;j<res.length();j++){
-                if(j + 1 == res.length() || res.charAt(j+1) != res.charAt(j)){
-                    item.append(count);
-                    item.append(res.charAt(j));
-                    count = 1;
-                }
-                else{
-                    count++;
-                }
-                
-            }
-            res = item.toString();
-        }
-        return res;
+
+public class Decode{
+  public int numDecoding(String s) {
+    if(s==null || s.length()==0 || s.charAt(0)=='0')
+    {
+        return 0;
     }
+    int num1=1;
+    int num2=1;
+    int num3=1;
+    for(int i=1;i<s.length();i++)
+    {
+        if(s.charAt(i)=='0')
+        {
+            if(s.charAt(i-1)=='1' || s.charAt(i-1)=='2')
+                num3 = num1;
+            else
+                return 0;
+        }
+        else
+        {
+            if(s.charAt(i-1)=='0' || s.charAt(i-1)>='3')
+                num3 = num2;
+            else
+            {
+                if(s.charAt(i-1)=='2' && s.charAt(i)>='7' && s.charAt(i)<='9')
+                    num3 = num2;
+                else
+                    num3 = num1+num2;
+            }
+        }
+        num1 = num2;
+        num2 = num3;
+    }
+    return num2;
+}
+    
 }
